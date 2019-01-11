da86bcbe3eedc42c232386bec9fa118eedb3b6c2
Monica Jianu <monica@ebi.ac.uk>
2019-01-11 16:58:24 +0000

    First commit containing Travis setup, as well as an initial set of tests for the browse experiments page.

---
+++ tests/test_browse_experiments.py
@@ -0,0 +1,51 @@
+import unittest
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+class BrowseExperiments(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.headless = True
+
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.set_window_size(1280, 1024)
+
+    def test_experiments_return_200(self):
+        driver = self.driver
+        driver.get('https://www-test.ebi.ac.uk/gxa/experiments')
+
+        driver.find_element_by_name("experiments-table_length").send_keys("25")
+        # driver.find_element_by_name("experiments-table_length").send_keys("All")
+
+        experiment_links = WebDriverWait(driver, 10).until(
+            expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR, "a[title='View in Expression Atlas']"))
+        )
+
+        self.assertGreaterEqual(len(experiment_links), 25)
+
+        for url in self.get_urls_from_html_elements(experiment_links):
+            with self.subTest(url=url):
+                print("checking url", url)
+                self.check_status_code_is_200(url)
+
+    def get_url_from_html_element(self, html_element):
+        return html_element.get_attribute('href')
+
+    def get_urls_from_html_elements(self, html_elements):
+        return [self.get_url_from_html_element(element) for element in html_elements]
+
+    def check_status_code_is_200(self, url):
+        response = requests.head(url)
+        self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
