b5f4d0452f856b90dc3c8f1ba102f0a43812120a
Monica Jianu <monica@ebi.ac.uk>
2019-01-15 12:04:05 +0000

    Added additional environment variable to indicate whether the test should be exhaustive or not. This will help speed up builds during development.

--- tests/test_browse_experiments.py
+++ tests/test_browse_experiments.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+
 import requests
 
 from selenium import webdriver
@@ -10,6 +11,7 @@ from selenium.webdriver.support import expected_conditions
 
 class BrowseExperiments(unittest.TestCase):
 
+    test_all: bool
     url_to_test: str
 
     def setUp(self):
@@ -21,20 +23,30 @@ class BrowseExperiments(unittest.TestCase):
 
         base_url = os.getenv('ATLAS_URL', 'https://www-test.ebi.ac.uk/gxa')
 
+        if os.getenv('TEST_ALL_EXPERIMENTS') == 'yes':
+            self.test_all = True
+        else:
+            self.test_all = False
+
         self.url_to_test = base_url + '/experiments'
 
     def test_experiments_return_200(self):
         driver = self.driver
         driver.get(self.url_to_test)
 
-        # driver.find_element_by_name('experiments-table_length').send_keys('25')
-        driver.find_element_by_name('experiments-table_length').send_keys('All')
+        if self.test_all:
+            driver.find_element_by_name('experiments-table_length').send_keys('All')
+        else:
+            driver.find_element_by_name('experiments-table_length').send_keys('25')
 
         experiment_links = WebDriverWait(driver, 10).until(
             expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR, "a[title='View in Expression Atlas']"))
         )
 
-        self.assertGreaterEqual(len(experiment_links), 3000)
+        if self.test_all:
+            self.assertGreaterEqual(len(experiment_links), 3000)
+        else:
+            self.assertGreaterEqual(len(experiment_links), 25)
 
         for url in self.get_urls_from_html_elements(experiment_links):
             with self.subTest(url=url):
