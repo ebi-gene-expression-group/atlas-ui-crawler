6900732a43a8bbd3f608ec7a571697e1c29497a1
Monica Jianu <monica@ebi.ac.uk>
2019-03-06 16:34:44 +0000

    Changed array return type to a set to remove duplicate URLs.

--- tests/utils/selenium_utils.py
+++ tests/utils/selenium_utils.py
@@ -3,4 +3,4 @@ def get_url_from_html_element(html_element):
 
 
 def get_urls_from_html_elements(html_elements):
-    return [get_url_from_html_element(element) for element in html_elements]
+    return set(get_url_from_html_element(element) for element in html_elements)
