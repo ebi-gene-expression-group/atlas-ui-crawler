1725055b40c5d760da8244df2404faddf71cebcc
Monica Jianu <monica@ebi.ac.uk>
2019-03-06 14:38:03 +0000

    Refactored method names, reorganised class variables. Added method for link in the FAQ page.

--- tests/gxa/test_help_pages.py
+++ tests/gxa/test_help_pages.py
@@ -8,8 +8,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-class BrowseExperiments(unittest.TestCase):
-    url_to_test: str
+class HelpPages(unittest.TestCase):
+    base_url: str
+    help_page_uri: str
+    faq_page_uri: str
 
     def setUp(self):
         options = Options()
@@ -18,13 +20,16 @@ class BrowseExperiments(unittest.TestCase):
         self.driver = webdriver.Chrome(options=options)
         self.driver.set_window_size(1280, 1024)
 
-        base_url = os.getenv('ATLAS_URL', 'https://www-test.ebi.ac.uk/gxa')
+        self.base_url = os.getenv('ATLAS_URL', 'https://www-test.ebi.ac.uk/gxa')
 
-        self.url_to_test = base_url + '/help/index.html'
+        self.help_page_uri = '/help/index.html'
+        self.faq_page_uri = '/FAQ.html'
 
-    def test_side_nav_links_return_200(self):
+    def test_side_nav_links(self):
         driver = self.driver
-        driver.get(self.url_to_test)
+        url_to_test = self.base_url + self.help_page_uri
+
+        driver.get(url_to_test)
 
         links = (driver
                  .find_element_by_id('main-content-area')
@@ -36,9 +41,11 @@ class BrowseExperiments(unittest.TestCase):
                 print('checking url', url)
                 self.assertEqual(get_request_status_code(url), 200)
 
-    def test_main_body_links_return_200(self):
+    def test_main_body_links(self):
         driver = self.driver
-        driver.get(self.url_to_test)
+        url_to_test = self.base_url + self.help_page_uri
+
+        driver.get(url_to_test)
 
         links = (driver
                  .find_element_by_id('main-content-area')
@@ -48,6 +55,23 @@ class BrowseExperiments(unittest.TestCase):
         # Testing both internal and external (e.g. GitHub, Bioconductor) links
         for url in get_urls_from_html_elements(links):
             with self.subTest(url=url):
+                print('checking url', url)
+                self.assertEqual(get_request_status_code(url), 200)
+
+    def test_faq_links(self):
+        driver = self.driver
+        url_to_test = self.base_url + self.faq_page_uri
+
+        driver.get(url_to_test)
+
+        links = (driver
+                 .find_element_by_id('main-content-area')
+                 .find_element_by_class_name('columns')
+                 .find_elements_by_tag_name('a'))
+
+        for url in get_urls_from_html_elements(links):
+            # JSTOR detects the requests are not coming from a human...
+            if "jstor.org" not in url:
                 print('checking url', url)
                 self.assertEqual(get_request_status_code(url), 200)
 
